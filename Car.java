1dafb1cb0b1e2d0a6f0de39e3c7a2d7c7cfc7318
ottowillborn <ottowillborn@gmail.com>
2025-02-07 20:28:02 -0800

    car movement

---
+++ Car.java
@@ -0,0 +1,152 @@
+public class Car {
+    private int x; // x position of the car on the grid
+    private int y; // y position of the car on the grid
+    private int speed; // speed of the car (could be in units per second)
+    private String direction; // direction of the car ("north", "south", "east", "west")
+    private boolean isMoving; // whether the car is moving or not
+
+    // Constructor to initialize the car's properties
+    public Car(int x, int y, int speed, String direction) {
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+        this.direction = direction;
+        this.isMoving = false; // Cars start off stopped
+    }
+
+    // Getters and setters for the car's attributes
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public void setDirection(String direction) {
+        this.direction = direction;
+    }
+
+    public boolean isMoving() {
+        return isMoving;
+    }
+
+    public void setMoving(boolean isMoving) {
+        this.isMoving = isMoving;
+    }
+
+    // Method to move the car based on its direction and speed
+    public void move() {
+        if (isMoving) {
+            switch (direction) {
+                case "north":
+                    x -= speed; // Move up
+                    break;
+                case "south":
+                    x += speed; // Move down
+                    break;
+                case "east":
+                    y += speed; // Move right
+                    break;
+                case "west":
+                    y -= speed; // Move left
+                    break;
+            }
+        }
+    }
+
+    public boolean willMoveOff() {
+        switch (direction) {
+            case "north":
+                if (this.x - speed < 0) {
+                    return true;
+                }else {
+                    return false;
+                }
+            case "south":
+                if (this.x + speed > 19) {
+                    return true;
+                }else {
+                    return false;
+                }
+                
+            case "east":
+                if (this.y + speed > 19) {
+                    return true;
+                }else {
+                    return false;
+                }
+            case "west":
+                x -= speed; // Move left
+                break;
+        }
+        return false;
+    }
+
+    public boolean canProceed(int[][] intersection) {
+        switch (direction) {
+            case "north":
+                return (intersection[12][7] == 'S') ?  false : true;
+            case "south":
+                return (intersection[12][7] == 'S') ?  false : true;
+            case "east":
+                return (intersection[12][12] == 'S') ?  false : true;
+            case "west":
+                x -= speed; // Move left
+                break;
+        }
+        return true;
+    }
+
+    public boolean isAtIntersection(int[][] intersection) {
+        switch (direction) {
+            case "north":
+                return (x==12 && y==11) ?  true : false;
+            case "south":
+                return (x==7 && y==8) ?  true : false;
+            case "east":
+                return (x==11 && y==7) ?  true : false;
+            case "west":
+                x -= speed; // Move left
+                break;
+        }
+        return true;
+    }
+
+    // Method to stop the car
+    public void stopCar() {
+        isMoving = false;
+    }
+
+    // Method to start the car
+    public void startCar() {
+        isMoving = true;
+    }
+
+    // Method to display the car's current status
+    @Override
+    public String toString() {
+        return "Car at (" + x + ", " + y + ") moving " + direction + " at speed " + speed + " units/sec.";
+    }
+}
+
